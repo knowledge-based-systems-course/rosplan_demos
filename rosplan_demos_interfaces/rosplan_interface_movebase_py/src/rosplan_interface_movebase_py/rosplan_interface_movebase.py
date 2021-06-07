a92a5a31cb88af26c8f9ad9e93fb307301055160
Oscar Lima <oscar.lima@dfki.de>
2021-06-07 12:46:34 +0200

    fix shebang and bug cancelAllGoals -> cancel_all_goals

--- rosplan_demos_interfaces/rosplan_interface_movebase_py/src/rosplan_interface_movebase_py/rosplan_interface_movebase.py
+++ rosplan_demos_interfaces/rosplan_interface_movebase_py/src/rosplan_interface_movebase_py/rosplan_interface_movebase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy, tf, actionlib
 import actionlib_msgs.msg
@@ -90,7 +90,7 @@ class RPMoveBasePy(RPActionInterface):
                 return False
         else:
             # timed out (failed)
-            self.action_client.cancelAllGoals()
+            self.action_client.cancel_all_goals()
             rospy.loginfo('KCL: ({}) action timed out'.format(self.params.name))
             return False
 
